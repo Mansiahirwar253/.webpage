f26e19a5a72bf1c31b828cb4ade29d127d223f6b
Mansiahirwar253 <67855410+Mansiahirwar253@users.noreply.github.com>
2020-07-12 13:21:35 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+ $('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+    
+    
+ });   
+});/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
